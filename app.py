c7afa0856e2f50ea0382d7c079a4bcf55731dc3f
Sameer Mohammad <99876168+sameermd33@users.noreply.github.com>
2024-08-31 12:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,5 +61,5 @@ def predict():
     
     return jsonify({"prediction": result_str})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
